0837392cce2e94ad5580ab1ad4ce6f1850a15165
kuba00739 <kuba.michal.n@gmail.com>
2021-05-26 08:44:28 +0200

    Deleted redundant script

--- router_iptables.sh
+++
@@ -1,6 +0,0 @@
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A INPUT -i wlo1 -j ACCEPT
-iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -t nat -A POSTROUTING -o wlp0s20f0u1 -j MASQUERADE
-iptables -A FORWARD -i wlp0s20f0u1 -o wlo1 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i wlo1 -o wlp0s20f0u1 -j ACCEPT
